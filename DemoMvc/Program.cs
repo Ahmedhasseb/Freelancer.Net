7a6255bb129dde51c288f27a881dc638c09fb2de
Ahmedhasseb <ahmedhasseb310@gmail.com>
2023-12-22 12:53:26 +0200

    FreelancerJopUseing>net

---
+++ DemoMvc/Program.cs
@@ -0,0 +1,71 @@
+using BLL.Interfaces;
+using BLL.Repostioes;
+using DAL.ContextConfiguration;
+using DAL.Moduls;
+using DemoMvc.Mapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoMvc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<Dbcontext>(option =>
+            {
+                option.UseSqlServer(builder.Configuration.GetConnectionString("defaltConnection"));
+            });
+
+            //services.AddScoped<IDepartmentRepostory, DepartmentRepostory>();
+            //services.AddScoped<IEmployeeRepository, EmployeeRepositorycs>();
+            builder.Services.AddScoped<IUnitOfWork, UniteOfWork>();
+
+            builder.Services.AddAutoMapper(e => e.AddProfile(new ClientProfile()));
+            builder.Services.AddAutoMapper(e => e.AddProfile(new GalleryProfile()));
+            builder.Services.AddAutoMapper(e => e.AddProfile(new HairArtistProfile()));
+
+            var app=builder.Build();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Gallery}/{action=Index}/{id?}");
+            });
+
+            app.Run();
+        }
+
+       
+    }
+}
